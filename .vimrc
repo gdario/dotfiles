caa99339c34af4a4df2ad6937c13bddbec09bffd
Giovanni d'Ario <giovanni.dario@gmail.com>
2017-11-06 13:36:52 +0100

    First commit.

---
+++ .vimrc
@@ -0,0 +1,65 @@
+syntax on
+set nocompatible              " be iMproved, required
+filetype off                  " required
+set ruler
+set ignorecase
+set smartcase
+set incsearch
+set wrap
+set linebreak
+set nolist
+set wrapmargin=0
+set modeline
+set foldmethod=indent
+set foldlevel=99
+set encoding=utf-8
+set colorcolumn=+1
+set wildmenu
+
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set textwidth=79
+set expandtab
+set autoindent
+set fileformat=unix
+
+set statusline+=%#warningmsg#
+set statusline+=%{SyntasticStatuslineFlag()}
+set statusline+=%*
+
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_auto_loc_list = 1
+let g:syntastic_check_on_open = 1
+let g:syntastic_check_on_wq = 0
+let g:syntastic_python_checkers = ['flake8']
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'godlygeek/tabular'
+Plugin 'plasticboy/vim-markdown'
+Plugin 'vim-syntastic/syntastic'
+Plugin 'Valloric/YouCompleteMe'
+Plugin 'vim-scripts/indentpython.vim'
+Plugin 'nvie/vim-flake8'
+Plugin 'tpope/vim-fugitive'
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
+" To ignore plugin indent changes, instead use:
+"filetype plugin on
+"
+" Brief help
+" :PluginList       - lists configured plugins
+" :PluginInstall    - installs plugins; append `!` to update or just :PluginUpdate
+" :PluginSearch foo - searches for foo; append `!` to refresh local cache
+" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal
+"
+" see :h vundle for more details or wiki for FAQ
+" Put your non-Plugin stuff after this line
+
---
+++ init.el
@@ -0,0 +1,74 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(column-number-mode t)
+ '(custom-enabled-themes nil)
+ '(package-selected-packages
+   (quote
+    (flx-ido projectile discover yafolding markdown-mode magit elpy)))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Add Melpa to the package repositories
+(package-initialize)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+;; Move around windows with SHIFT + arrow
+(windmove-default-keybindings)
+
+;; Remove the toolbar
+(tool-bar-mode -1)
+
+; Activate ido-mode, but ignore it when writing files
+(require 'ido)
+(ido-mode t)
+(define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil)
+
+;; Use markdown-mode
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+; Use visual line mode
+(global-visual-line-mode t)
+
+;; Use pdflatex by default
+(setq TeX-PDF-mode t)
+
+;; Create shortcut for magit-status
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Use company-mode globally
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Enable Elpy
+(package-initialize)
+(elpy-enable)
+
+;; Enable yafolding
+(setq yafolding-mode t)
+
+;; Enable projectile
+(projectile-global-mode)
