d1208dfcddae42dabc7564dc23f212c015b090fa
Giovanni d'Ario <giovanni.dario@gmail.com>
2017-12-16 12:06:46 +0100

    Change default UNIX font to monospace regular 12

--- init.el
+++ init.el
@@ -68,7 +68,7 @@
 ;; Select the font-type based on the system you are working on
 (setq my-preferred-font
       (cond ((eq system-type 'darwin) "Monaco-13")
-	    ((eq system-type 'gnu/linux) "mono-13")
+	    ((eq system-type 'gnu/linux) "monospace-12")
 	    (t nil)))
 (when my-preferred-font
   (set-frame-font my-preferred-font nil t))
