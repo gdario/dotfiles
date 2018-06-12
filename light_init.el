5125a8157c17cba4716fce081be3752bf88d73f1
Giovanni d'Ario <giovanni.dario@novartis.com>
2018-06-12 10:18:25 +0200

    Add a light init.el

---
+++ light_init.el
@@ -0,0 +1,61 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; Add Melpa to the package repositories
+(package-initialize)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+;; Move around windows with SHIFT + arrow
+(windmove-default-keybindings)
+
+;; Remove the toolbar
+(tool-bar-mode -1)
+
+;; Use an indentation style for ESS that mimics the one of RStudio
+(setq ess-default-style 'DEFAULT)
+
+;; Use visual line mode
+(global-visual-line-mode t)
+
+;; Use pdflatex by default
+(setq TeX-PDF-mode t)
+
+;; Create shortcut for magit-status
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Use company-mode globally
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Enable Elpy
+(package-initialize)
+(elpy-enable)
+
+;; Enable flycheck real time syntax checking in Elpy
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Don't create backup files
+(setq make-backup-files nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(package-selected-packages
+   (quote
+    (elpygen csv-mode markdown-mode ess ssh magit elpy)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
