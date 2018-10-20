f9c9646511d9aeead0f11e00e447812ff193abad
Giovanni d'Ario <giovanni.dario@gmail.com>
2018-10-20 15:27:32 +0200

    Include helm and projectile Remove extra themes and use the default font on the mac, only a bit larger.

--- init.el
+++ init.el
@@ -20,11 +20,24 @@
 ;; Create shortcut for magit-status
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; ;; Use company-mode globally
-;; (add-hook 'after-init-hook 'global-company-mode)
+;; Use company-mode globally
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Enable helm (install with M-x package-install helm)
+(package-initialize)
+(require 'helm-config)
+(helm-mode 1)
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+;; Access the buffer list via helm
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+
+;; Enable projectile
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 ;; Enable Elpy
-(package-initialize)
 (elpy-enable)
 
 ;; Enable flycheck real time syntax checking in Elpy
@@ -36,22 +49,31 @@
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "-i --simple-prompt")
 
-;; Select the font-type based on the system you are working on
-(setq my-preferred-font
-      (cond ((eq system-type 'darwin) "Monaco-13")
-	    ((eq system-type 'gnu/linux) "monospace-12")
-	    (t nil)))
-(when my-preferred-font
-  (set-frame-font my-preferred-font nil t))
-
 (setq ispell-program-name
       (cond ((eq system-type 'darwin) "/usr/local/bin/ispell")
-	    ((eq system-type 'gnu/linux) "/usr/bin/aspell")
 	    (t nil)))
 
 ;; Don't create backup files
 (setq make-backup-files nil)
 
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
+;; Select the font-type based on the system you are working on
+(setq my-preferred-font
+      (cond ((eq system-type 'darwin) "Menlo-13")
+	    ((eq system-type 'gnu/linux) "monospace-12")
+	    (t nil)))
+(when my-preferred-font
+  (set-frame-font my-preferred-font nil t))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (projectile monokai-theme magit helm ess elpy csv-mode auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
