08bf942df8bce6c8e46c7b950929f3aa6b5b6cde
Giovanni d'Ario <giovanni.dario@roche.com>
2020-04-02 10:30:09 +0200

    Bring back sphinx-doc

--- init.el
+++ init.el
@@ -1,10 +1,15 @@
+;; Add Melpa to the package repositories
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+;; Initialize packages
 (package-initialize)
 
-;; ----------------------------------------------------------------------------
-;;  		         General configuration
-;; ----------------------------------------------------------------------------
+;; Initialize sphinx-doc-mode
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (require 'sphinx-doc)
+	    (sphinx-doc-mode t)))
 
 ;; Show column numbers by default
 (setq column-number-mode t)
@@ -12,14 +17,49 @@
 ;; Show matching parentheses by default
 (setq show-paren-mode 1)
 
+;; Package: fill-column-index: set the 80 column inidicator
+(setq fci-rule-column 80)
+
 ;; Move around windows with SHIFT + arrow
 (windmove-default-keybindings)
 
 ;; Remove the toolbar
 (tool-bar-mode -1)
 
+;; Use an indentation style for ESS that mimics the one of RStudio
+(setq ess-default-style 'DEFAULT)
+
 ;; Use visual line mode
 (global-visual-line-mode t)
+
+;; Use pdflatex by default
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-save-query nil)
+(setq TeX-PDF-mode t)
+
+;; Stan support
+;; M-x package-install [RET] stan-mode [RET]
+;; M-x package-install [RET] company-stan [RET]
+;; M-x package-install [RET] eldoc-mode [RET]
+;; M-x package-install [RET] flycheck-mode [RET]
+;; M-x package-install [RET] stan-snippets [RET]
+
+;; Create shortcut for magit-status
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Use company-mode globally
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Enable projectile
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;; Enable ido-mode
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
 
 ;; Shortcut for org-agenda
 (global-set-key "\C-ca" 'org-agenda)
@@ -35,10 +75,13 @@
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
-;; Enable ido-mode
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
+;; Use python -m json.tool to prettify JSON files
+(defun format-json ()
+  (interactive)
+  (save-excursion
+    (shell-command-on-region (mark) (point) "python -m json.tool" (buffer-name) t)
+    )
+  )
 
 (setq ispell-program-name
       (cond ((eq system-type 'darwin) "/usr/local/bin/ispell")
@@ -59,4 +102,3 @@
 
 (when my-preferred-font
   (set-frame-font my-preferred-font nil t))
-
