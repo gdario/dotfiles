dff79dee59f05f96e8428ecda0e5ef25343bf0a1
Giovanni d'Ario <giovanni.dario@roche.com>
2020-07-03 09:48:40 +0200

    Install the neovim nightly build by default.

--- install_neovim.sh
+++ install_neovim.sh
@@ -1,4 +1,4 @@
 mkdir ~/.config/nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ~/dotfiles/init.vim ~/.config/nvim/
-brew install neovim
+brew install --HEAD neovim
