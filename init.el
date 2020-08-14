05760f4a055b76b999614b2404b173607e64ab60
Giovanni d'Ario <giovanni.dario@roche.com>
2020-08-14 15:27:31 +0200

    Add default font for the Mac.

--- init.el
+++ init.el
@@ -40,6 +40,10 @@
 ;; store all backup and autosave files in the .saves dir
 (setq backup-directory-alist `(("." . "~/.saves")))
 
+;; Set default font
+(set-frame-font "Monaco-13" nil t)
+
+;; Set default encoding
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
@@ -159,11 +163,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(package-selected-packages
-   '(exec-path-from-shell poly-R poly-markdown ess elpy flycheck company markdown-mode projectile flx-ido magit fill-column-indicator use-package)))
+   '(exec-path-from-shell poly-R poly-markdown ess elpy flycheck company markdown-mode projectile flx-ido magit fill-column-indicator use-package))
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Monaco")))))
+ )
