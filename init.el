eca5d96ea02eee5aa536b65f3f4238ef7fa6fd62
Giovanni d'Ario <giovanni.dario@roche.com>
2020-12-07 20:52:40 +0100

    Remove font specification and wombat theme.

--- init.el
+++ init.el
@@ -41,7 +41,7 @@
 (setq backup-directory-alist `(("." . "~/.saves")))
 
 ;; Set default font
-(set-frame-font "Monaco-13" nil t)
+;; (set-frame-font "Monaco-13" nil t)
 
 ;; Set default encoding
 (prefer-coding-system 'utf-8)
@@ -87,8 +87,7 @@
 ;;   :ensure t)
 
 (use-package evil
-  :ensure t
-  :config)
+  :ensure t)
 
 (use-package fill-column-indicator
   :ensure t
@@ -171,7 +170,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(wombat))
  '(elpy-rpc-python-command "python3")
  '(elpy-shell-starting-directory 'current-directory)
  '(global-visual-line-mode t)
