86ecbf2feaf32ca5135483ac69ddea782b83d7b9
Giovanni d'Ario <giovanni.dario@roche.com>
2021-03-28 16:09:10 +0200

    Add stan configuration details.

--- init.el
+++ init.el
@@ -201,37 +201,62 @@
   :defer t
   :ensure t)
 
-;; stan-mode.el
+;;; stan-mode.el
 (use-package stan-mode
   :mode ("\\.stan\\'" . stan-mode)
   :hook (stan-mode . stan-mode-setup)
+  ;;
   :config
+  ;; The officially recommended offset is 2.
   (setq stan-indentation-offset 2))
 
-;; company-stan.el
+;;; company-stan.el
 (use-package company-stan
   :hook (stan-mode . company-stan-setup)
+  ;;
   :config
+  ;; Whether to use fuzzy matching in `company-stan'
   (setq company-stan-fuzzy nil))
 
-;; eldoc-stan.el
+;;; eldoc-stan.el
 (use-package eldoc-stan
   :hook (stan-mode . eldoc-stan-setup)
-  :config)
+  ;;
+  :config
+  ;; No configuration options as of now.
+  )
 
-;; flycheck-stan.el
+;;; flycheck-stan.el
 (use-package flycheck-stan
   ;; Add a hook to setup `flycheck-stan' upon `stan-mode' entry
   :hook ((stan-mode . flycheck-stan-stanc2-setup)
          (stan-mode . flycheck-stan-stanc3-setup))
   :config
+  ;; A string containing the name or the path of the stanc2 executable
+  ;; If nil, defaults to `stanc2'
   (setq flycheck-stanc-executable nil)
+  ;; A string containing the name or the path of the stanc2 executable
+  ;; If nil, defaults to `stanc3'
   (setq flycheck-stanc3-executable nil))
 
-;; stan-snippets.el
+;;; stan-snippets.el
 (use-package stan-snippets
   :hook (stan-mode . stan-snippets-initialize)
-  :config)
+  ;;
+  :config
+  ;; No configuration options as of now.
+  )
+
+;; ;;; ac-stan.el (Not on MELPA; Need manual installation)
+;; (use-package ac-stan
+;;   :load-path "path-to-your-directory/ac-stan/"
+;;   ;; Delete the line below if using.
+;;   :disabled t
+;;   :hook (stan-mode . stan-ac-mode-setup)
+;;   ;;
+;;   :config
+;;   ;; No configuration options as of now.
+;;   )
 
 (use-package exec-path-from-shell
   :ensure t
