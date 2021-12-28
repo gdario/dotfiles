b19ad73fa8de9b2de03869183a090229c90eb697
Giovanni d'Ario <giovanni.dario@roche.com>
2021-12-28 19:19:58 +0100

    Add the fill column indicator variable.

--- init_light.el
+++ init_light.el
@@ -140,6 +140,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(display-fill-column-indicator-column 80)
  '(ess-R-font-lock-keywords
    '((ess-R-fl-keyword:keywords . t)
      (ess-R-fl-keyword:constants . t)
