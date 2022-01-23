f976c1af6a1e7b0a2743c8cfa32671fe58510fd9
Giovanni d'Ario <giovanni.dario@roche.com>
2022-01-23 16:29:08 +0100

    Change the default style for C and C++.

--- init_light.el
+++ init_light.el
@@ -181,6 +181,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-default-style '((c-mode . "") (c++-mode . "")))
  '(column-number-mode t)
  '(display-fill-column-indicator-column 80)
  '(elpy-shell-starting-directory 'current-directory)
