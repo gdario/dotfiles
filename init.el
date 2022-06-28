6b79ad320c9b4bf544f40c2cd96dcd192668919f
Giovanni d'Ario <giovanni.dario@roche.com>
2022-06-28 13:36:46 +0200

    Change the capture template

--- init.el
+++ init.el
@@ -70,7 +70,13 @@
   (setq org-clock-persist 'history)
   (org-clock-persistence-insinuate)
   (setq org-log-done t)
-  (setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$")))
+  (setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
+  (setq org-default-notes-file (concat org-directory "/notes.org"))
+  (setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+         "* %?\nEntered on %U\n  %i\n  %a"))))
 
 (use-package ssh)
 
