092423e23537003c50d8cb7dda56622b9d9ea688
Giovanni d'Ario <giovanni.dario@gmail.com>
2022-09-24 09:47:14 +0200

    Remove outdated files.

--- install_neovim_linux.sh
+++
@@ -1,4 +0,0 @@
-mkdir ~/.config/nvim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp ~/dotfiles/init.vim ~/.config/nvim/
-sudo snap install --edge nvim --classic
