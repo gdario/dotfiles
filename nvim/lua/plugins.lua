9f87a542c1f65a3f382c8111844c0766b7091971
Giovanni d'Ario <giovanni.dario@roche.com>
2023-06-05 16:16:16 +0200

    Remove infrequently used plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,7 +1,5 @@
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-    use 'eigenfoo/stan-vim'
-    use 'preservim/vim-markdown'
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
