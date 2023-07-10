999e18e3d38398519de4c3a00d0a2fadc24cbc82
Giovanni d'Ario <giovanni.dario@roche.com>
2023-07-10 09:41:30 +0200

    set python-shell-completion-native-enable to nil

--- emacs/init_minimal.el
+++ emacs/init_minimal.el
@@ -31,7 +31,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fill-column 80))
+ '(fill-column 80)
+ '(python-shell-completion-native-enable nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
