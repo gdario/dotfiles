f680bf0d36cd0d82813556c6be7dc88e01d06fe0
Giovanni d'Ario <giovanni.dario@gmail.com>
2023-08-23 17:22:45 +0200

    Update init.el
    Comment out much of the init.el file and make Elpy's indentation work again.

--- emacs/init.el
+++ emacs/init.el
@@ -6,7 +6,7 @@
 
 ;;; Commentary:
 
-;; This is my personal Emacs configuration.  Nothing more, nothing less.
+;; A mostly stolen init file.
 
 ;;; Code:
 (require 'package)
@@ -39,11 +39,10 @@
 ;; Startup settings
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
-;; (setq visible-bell t)
-(set-default 'truncate-lines t)
+;; (set-default 'truncate-lines t)
 (show-paren-mode t)
 (column-number-mode t)
-(electric-pair-mode t)
+;; (electric-pair-mode t)
 
 ;; Set the font and the location of the spell-checker based on the operating
 ;; system
@@ -59,95 +58,100 @@
 (add-hook 'prog-mode-hook #'hs-minor-mode)
 
 ;; Require a newline at end of the file
-(setq require-final-newline t)
-
-;; Setup org-mode
-(use-package org
-  :bind
-  (("C-c a" . org-agenda)
-  ("C-c l" . org-store-link)
-  ("C-c c" . org-capture)
-  ("C-c b" . org-switchb))
-  :config
-  (electric-indent-mode -1)
-  (setq org-clock-persist 'history)
-  (org-clock-persistence-insinuate)
-  (setq org-log-done t))
+;; (setq require-final-newline t)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)
    (R . t)))
 
-(use-package exec-path-from-shell
-  :defer t
-  :if (memq window-system '(mac ns x))
-  :config (exec-path-from-shell-initialize))
+;; Setup org-mode
+(use-package org
+  :bind
+  (("C-c a" . org-agenda)
+  ("C-c l" . org-store-link)
+  ("C-c c" . org-capture))
+  ;; ("C-c b" . org-switchb))
+  :config
+  ;; (electric-indent-mode -1)
+  ;; (setq org-clock-persist 'history)
+  ;; (org-clock-persistence-insinuate)
+  (setq org-log-done t))
 
 (use-package elpy
-  :defer t
-  :if (memq window-system '(mac ns x))
-  :config
-  (setq python-shell-completion-native-enable nil)
-  :init
-  (advice-add 'python-mode :before 'elpy-enable))
-
-(use-package company
   :ensure t
-  :config
-  (global-company-mode t))
-
-(use-package eglot)
-
-(use-package ssh
-  :defer t)
-
-(use-package fill-column-indicator
-  :defer t)
-
-(use-package ido
   :defer t
-  :config
-  (setq ido-enable-flex-matching t)
-  (setq ido-everywhere t)
-  (ido-mode 1))
-
-(use-package magit
-  :defer t
-  :bind (("C-x g" . magit-status)))
-
-(use-package markdown-mode
-  :defer t
-  :commands (markdown-mode gfm-mode)
-  :mode
-  (("README\\.md\\'" . gfm-mode)
-   ("\\.md\\'" . markdown-mode)
-   ("\\.markdown\\'" . markdown-mode))
   :init
-  (setq markdown-command "multimarkdown"))
+  (elpy-enable))
 
-(use-package ess
-  :defer t
-  :config
-  (setq ess-style 'RStudio))
+;; (use-package exec-path-from-shell
+;;   :defer t
+;;   :if (memq window-system '(mac ns x))
+;;   :config (exec-path-from-shell-initialize))
 
-(use-package poly-markdown
-  :defer t)
+;; (use-package elpy
+;;   :defer t
+  ;; :if (memq window-system '(mac ns x))
+  ;; :config
+  ;; (setq python-shell-completion-native-enable nil)
+  ;; :init
+  ;; (advice-add 'python-mode :before 'elpy-enable))
 
-(use-package poly-R
-  :defer t)
+;; (use-package company
+;;   :ensure t
+;;   :config
+;;   (global-company-mode t))
+
+;; (use-package eglot)
+
+;; (use-package ssh
+;;   :defer t)
+
+;; (use-package fill-column-indicator
+;;   :defer t)
+
+;; (use-package ido
+;;   :defer t
+;;   :config
+;;   (setq ido-enable-flex-matching t)
+;;   (setq ido-everywhere t)
+;;   (ido-mode 1))
+
+;; (use-package magit
+;;   :defer t
+;;   :bind (("C-x g" . magit-status)))
+
+;; (use-package markdown-mode
+;;   :defer t
+;;   :commands (markdown-mode gfm-mode)
+;;   :mode
+;;   (("README\\.md\\'" . gfm-mode)
+;;    ("\\.md\\'" . markdown-mode)
+;;    ("\\.markdown\\'" . markdown-mode))
+;;   :init
+;;   (setq markdown-command "multimarkdown"))
+
+;; (use-package ess
+;;   :defer t
+;;   :config
+;;   (setq ess-style 'RStudio))
+
+;; (use-package poly-markdown
+;;   :defer t)
+
+;; (use-package poly-R
+;;   :defer t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elpy-rpc-python-command "python3")
- '(elpy-shell-starting-directory 'current-directory)
+ '(column-number-mode t)
+ '(display-line-numbers-type nil)
  '(fill-column 80)
- '(package-selected-packages '(auto-package-update hs-minor-mode use-package)))
-
-;;; init.el ends here
+ '(package-selected-packages '(auto-package-update hs-minor-mode use-package))
+ '(python-shell-completion-native-enable nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
