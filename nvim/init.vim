2504e6d7e8b73689f017a84316856eb4c9dba086
Giovanni d'Ario <giovanni.dario@gmail.com>
2023-10-29 09:56:50 +0100

    Add kickstarter nvim folder

--- nvim/init.vim
+++
@@ -1,7 +0,0 @@
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc
-
-lua require('plugins')
-lua require('init')
-lua require('lsp_config')
--- nvim/lua/plugins.lua
+++
@@ -1,8 +0,0 @@
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-    use 'neovim/nvim-lspconfig'
-end)
