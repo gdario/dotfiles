85c3eff7e4c46651279ced19bb9ce1f114a3769e
Giovanni d'Ario <giovanni.dario@gmail.com>
2024-01-06 17:43:53 +0100

    Add exec-path-from-shell to init_eglot.el
    Without it, GUI Emacs cannot find the python-lsp-server.
    N.B.: the server is installed via pip3 install --user ...

--- emacs/init_eglot.el
+++ emacs/init_eglot.el
@@ -74,6 +74,84 @@
 (use-package magit
   :bind (("C-x g" . magit-status)))
 
+ackages
+;;(use-package auto-package-update
+;;  :config
+;;  (setq auto-package-update-delete-old-versions t)
+;;  (setq auto-package-update-hide-results t)
+;;  (auto-package-update-maybe))
+
+;; Store all backup and autosave files in the .saves dir
+(setq backup-directory-alist `(("." . "~/.saves")))
+
+;; Startup settings
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+;; (setq visible-bell t)
+;; (set-default 'truncate-lines t)
+(show-paren-mode t)
+(column-number-mode t)
+;; (electric-pair-mode t)
+
+;; Set the font and the location of the spell-checker based on the operating
+;; system
+(if (eq system-type 'darwin)
+    (progn (set-frame-font "Monaco-13" nil t)
+     (setq ispell-program-name "/usr/local/bin/ispell"))
+  (set-frame-font "Monospace-11" nil t))
+
+;; Enable visual-line mode in all text modes
+(add-hook 'text-mode-hook #'visual-line-mode)
+;; Enable hs-minor mode on all programming modes
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+;; Enable Eglot by default in Python buffers
+(add-hook 'python-mode-hook 'eglot-ensure)
+
+(keymap-global-set "C-M-z" 'zap-up-to-char)
+
+;; Require a newline at end of the file
+;; (setq require-final-newline t)
+
+;; Setup org-mode
+(use-package org
+  :bind
+  (("C-c a" . org-agenda)
+   ("C-c l" . org-store-link)
+   ("C-c c" . org-capture)
+   ("C-c b" . org-switchb))
+  :config
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((python . t)
+     (R . t)))
+  (electric-indent-mode -1)
+  ;; (setq org-clock-persist 'history)
+  ;; (org-clock-persistence-insinuate)
+  (setq org-babel-python-command "python3")
+  (setq org-log-done t))
+
+;; (use-package flycheck
+;;   :init (global-flycheck-mode))
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode t))
+
+(use-package ssh
+  :defer t)
+
+;; Needed on OSX to make Eglot find the python-lsp-server
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
+(use-package ssh
+  :ensure t
+  :defer t)
+
 ;; (use-package markdown-mode
 ;;   :defer t
 ;;   :commands (markdown-mode gfm-mode)
