4aa0bd78eda83f9e86a99c8f7043887261a9f88f
Giovanni d'Ario <giovanni.dario@roche.com>
2025-05-18 16:30:24 +0200

    Update init_minimal.el

--- emacs/init_minimal.el
+++ emacs/init_minimal.el
@@ -1,47 +1,81 @@
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
-
-(package-initialize)
-
-;; store all backup and autosave files in the .saves dir
-(setq backup-directory-alist `(("." . "~/.saves")))
-
-;; Set the default font, based on the OS
-(if (eq system-type 'darwin)
-    (progn (set-frame-font "Monaco-13" nil t)
-     (setq ispell-program-name "/usr/local/bin/ispell"))
-  (set-frame-font "Monospace-11" nil t))
-
-(set-default 'truncate-lines t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(setq backup-directory-alist '(("." . "~/.saves")))
+(if window-system (set-frame-font "Menlo 13" nil t))
+(setq inhibit-startup-message t)
+(column-number-mode t)
+(windmove-default-keybindings 'super)
 (tool-bar-mode -1)
-(show-paren-mode t)
-(electric-pair-mode t)
 
-;; Enable visual-line mode in all text modes
-(add-hook 'text-mode-hook #'visual-line-mode)
+(use-package org
+  :bind
+  (("C-c a" . org-agenda)
+   ("C-c l" . org-store-link)
+   ("C-c c" . org-capture))
+  :init
+  (require 'org-tempo)
+  :hook
+  (org-mode . visual-line-mode)
+  :config
+  (setq org-directory "~/Documents/org")
+  (setq org-default-notes-file (concat org-directory "/notes.org")))
 
-;; Enable hs-minor mode on all programming modes
-(add-hook 'prog-mode-hook #'hs-minor-mode)
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
 
-;; Configure org-mode
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
-(setq org-log-done t)
+(use-package pyvenv
+  :defer t
+  :ensure t)
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)
-   (R . t)))
+(use-package python-mode
+  :defer t
+  :init
+  (setq python-shell-completion-native-enable nil))
+
+(use-package csv-mode
+  :ensure t
+  :defer t
+  :hook
+  (csv-mode . csv-align-mode))
+
+;; (use-package magit
+;;   :ensure t
+;;   :defer t
+;;   :bind (("C-x g" . magit-status)))
+
+(use-package markdown-mode
+  :ensure t
+  :defer t
+  :commands (markdown-mode gfm-mode)
+  :mode
+  (("README\\.md\\'" . gfm-mode)
+   ("\\.md\\'" . markdown-mode)
+   ("\\.markdown\\'" . markdown-mode))
+  :init
+  (setq markdown-command "multimarkdown")
+  :hook
+  (markdown-mode . visual-line-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(display-line-numbers-type nil)
- '(fill-column 80)
- '(python-shell-completion-native-enable nil)
- '(tool-bar-mode nil))
+ '(org-export-backends '(ascii html icalendar md odt))
+ '(package-selected-packages nil)
+ '(sql-connection-alist
+   '(("trinetx-pca" (sql-product 'postgres) (sql-user "dariog")
+      (sql-server "redshift-01-us.dap.apollo.roche.com")
+      (sql-database "trinetx_pca") (sql-port 5439))
+     ("pincai" (sql-product 'postgres) (sql-user "dariog")
+      (sql-server "redshift-02-us.dap.apollo.roche.com")
+      (sql-database "premier_phd_ac") (sql-port 5439)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
