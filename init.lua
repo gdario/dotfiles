19ec610c781733c8b8f846198d9e484a2e35eb34
Giovanni d'Ario <giovanni.dario@roche.com>
2025-07-23 17:58:04 +0200

    Add conform.nvim

--- init.lua
+++ init.lua
@@ -59,64 +59,62 @@ local plugins = {
 --   { "nvim-tree/nvim-tree.lua" },
 --   { "nvim-telescope/telescope.nvim" },
 --   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "shaunsingh/solarized.nvim" },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
---   { "mason-org/mason.nvim" },
---   { "neovim/nvim-lspconfig" },
---   { "mason-org/mason-lspconfig.nvim" },
---   { "stevearc/conform.nvim" },
---   {
---     'saghen/blink.cmp',
---     -- optional: provides snippets for the snippet source
---     dependencies = { 'rafamadriz/friendly-snippets' },
--- 
---     -- use a release tag to download pre-built binaries
---     version = '1.*',
---     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
---     -- build = 'cargo build --release',
---     -- If you use nix, you can build from source using latest nightly rust with:
---     -- build = 'nix run .#build-plugin',
--- 
---     ---@module 'blink.cmp'
---     ---@type blink.cmp.Config
---     opts = {
---       -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
---       -- 'super-tab' for mappings similar to vscode (tab to accept)
---       -- 'enter' for enter to accept
---       -- 'none' for no mappings
---       --
---       -- All presets have the following mappings:
---       -- C-space: Open menu or open docs if already open
---       -- C-n/C-p or Up/Down: Select next/previous item
---       -- C-e: Hide menu
---       -- C-k: Toggle signature help (if signature.enabled = true)
---       --
---       -- See :h blink-cmp-config-keymap for defining your own keymap
---       keymap = { preset = 'default' },
--- 
---       appearance = {
---         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
---         -- Adjusts spacing to ensure icons are aligned
---         nerd_font_variant = 'mono'
---       },
--- 
---       -- (Default) Only show the documentation popup when manually triggered
---       completion = { documentation = { auto_show = false } },
--- 
---       -- Default list of enabled providers defined so that you can extend it
---       -- elsewhere in your config, without redefining it, due to `opts_extend`
---       sources = {
---         default = { 'lsp', 'path', 'snippets', 'buffer' },
---       },
--- 
---       -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
---       -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
---       -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
---       --
---       -- See the fuzzy documentation for more information
---       fuzzy = { implementation = "prefer_rust_with_warning" }
---     },
---     opts_extend = { "sources.default" }
---   },
+  { "mason-org/mason.nvim" },
+  { "neovim/nvim-lspconfig" },
+  { "mason-org/mason-lspconfig.nvim" },
+  { "stevearc/conform.nvim", opts = {} },
+  {
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
+
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'default' },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+  },
 }
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -133,7 +131,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(plugins)
 
--- vim.cmd.colorscheme("gruvbox")
+vim.cmd.colorscheme("catppuccin")
 -- require("lualine").setup()
 -- require("nvim-tree").setup()
 -- require("telescope").setup()
@@ -150,10 +148,10 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
 
 -- Mason & LSP
--- require("mason").setup()
--- require("mason-lspconfig").setup({ ensure_installed = { "pylsp", "r_language_server" } })
+require("mason").setup()
+require("mason-lspconfig").setup({ ensure_installed = { "pylsp" } })
 
--- require("conform").setup({
---   default_format_opts = { lsp_format = "fallback" },
--- })
+require("conform").setup({
+  default_format_opts = { lsp_format = "fallback" },
+})
 
