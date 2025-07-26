5a6f20afc3a4a36df7cc7499dc87fbbbf5868a10
Giovanni d'Ario <giovanni.dario@roche.com>
2025-07-26 18:22:19 +0200

    Fix the Emacs init and the .vimrc_light.

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,11 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (setq backup-directory-alist '(("." . "~/.saves")))
-(if window-system (set-frame-font "Menlo 13" nil t))
+(if window-system
+    (progn
+      (set-frame-font "Menlo 13" nil t)
+      (load-theme 'spacemacs-light t)))
+
 (setq inhibit-startup-message t)
 (column-number-mode t)
 (windmove-default-keybindings 'super)
@@ -13,8 +17,11 @@
           word-wrap t)))
 
 (add-hook 'prog-mode-hook '(lambda ()
-    (setq truncate-lines t
+    (setq visual-line-mode t
           word-wrap nil)))
+
+(use-package spacemacs-theme
+  :ensure t)
 
 (use-package org
   :bind
@@ -23,8 +30,10 @@
    ("C-c c" . org-capture))
   :init
   (require 'org-tempo)
+  (if window-system (setq org-hide-emphasis-markers t))
   :hook
   (org-mode . visual-line-mode)
+  (org-mode . (lambda () (setq electric-indent-local-mode -1)))
   :config
   (setq org-directory "~/Documents/org")
   (setq org-default-notes-file (concat org-directory "/notes.org")))
@@ -35,11 +44,11 @@
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
 
-(use-package corfu
-  :ensure t
-  :defer t
-  :init
-  (global-corfu-mode))
+;; (use-package corfu
+;;   :ensure t
+;;   :defer t
+;;   :init
+;;   (global-corfu-mode))
 
 (use-package pyvenv
   :defer t
@@ -50,7 +59,8 @@
   :init
   (setq python-shell-completion-native-enable nil)
   :hook
-  (python-mode . eglot-ensure))
+  (python-mode . eglot-ensure)
+  (python-mode . completion-preview-mode))
 
 (use-package magit
   :ensure t
@@ -84,9 +94,9 @@
   :ensure t
   :defer t)
 
-(use-package auctex
-  :ensure t
-  :defer t)
+;; (use-package auctex
+;;   :ensure t
+;;   :defer t)
 
 (use-package ess
   :ensure t
@@ -120,11 +130,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("9af2b1c0728d278281d87dc91ead7f5d9f2287b1ed66ec8941e97ab7a6ab73c0"
+     default))
  '(org-export-backends '(ascii html icalendar md odt))
- '(package-selected-packages
-   '(auctex corfu ess exec-path-from-shell magit markdown-mode nov poly-R
-	    poly-markdown poly-noweb pyvenv sicp toml-mode vertico
-	    yaml-mode))
+ '(package-selected-packages nil)
  '(sql-connection-alist
    '(("trinetx-pca" (sql-product 'postgres) (sql-user "dariog")
       (sql-server "redshift-01-us.dap.apollo.roche.com")
--- init.lua
+++ init.lua
@@ -1,157 +1,95 @@
--- Basic settings
-vim.opt.number = true
--- vim.opt.relativenumber = true
--- vim.opt.mouse = "a"
--- vim.opt.showmode = false
--- vim.opt.spelllang = "en_gb"
+-- Set <space> as the leader key
+-- See `:help mapleader`
+-- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
 
--- Leader key
--- vim.g.mapleader = ","
+-- [[ Setting options ]] See `:h vim.o`
+-- NOTE: You can change these options as you wish!
+-- For more options, you can see `:help option-list`
+-- To see documentation for an option, you can use `:h 'optionname'`, for example `:h 'number'`
+-- (Note the single quotes)
 
--- Use nvim-tree
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+-- Print the line number in front of each line
+vim.o.number = true
 
--- Use system clipboard
--- vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
+-- Use relative line numbers, so that it is easier to jump with j, k. This will affect the 'number'
+-- option above, see `:h number_relativenumber`
+vim.o.relativenumber = true
 
--- Display settings
--- vim.opt.termguicolors = true
--- vim.o.background = "dark" -- or "light"
-
--- Scrolling and UI settings
-vim.opt.cursorline = true
--- vim.opt.cursorcolumn = true
--- vim.opt.signcolumn = 'yes'
-vim.opt.wrap = false
--- vim.opt.sidescrolloff = 8
-
--- Title
--- vim.opt.title = true
--- vim.opt.titlestring = "nvim"
-
--- Indentation
-vim.opt.tabstop = 8
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-
--- Search configuration
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.gdefault = true
-
--- Open new split panes to right and below
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- LSP
-vim.lsp.inlay_hint.enable(true)
-
--- Plugins
-local plugins = {
---   { "nvim-lua/plenary.nvim" },
---   { "nvim-tree/nvim-web-devicons" },
---   { "ellisonleao/gruvbox.nvim" },
---   { "nvim-lualine/lualine.nvim" },
---   { "nvim-tree/nvim-tree.lua" },
---   { "nvim-telescope/telescope.nvim" },
---   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "shaunsingh/solarized.nvim" },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { "mason-org/mason.nvim" },
-  { "neovim/nvim-lspconfig" },
-  { "mason-org/mason-lspconfig.nvim" },
-  { "stevearc/conform.nvim", opts = {} },
-  {
-    'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets' },
-
-    -- use a release tag to download pre-built binaries
-    version = '1.*',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
-
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
-      },
-
-      -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = false } },
-
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" }
-    },
-    opts_extend = { "sources.default" }
-  },
-}
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(plugins)
-
-vim.cmd.colorscheme("catppuccin")
--- require("lualine").setup()
--- require("nvim-tree").setup()
--- require("telescope").setup()
-
--- Treesitter
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "python", "r", "rust", "c" },
-  sync_install = false,
-  auto_install = true,
-  highlight = { enable = true, },
-})
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 99
-
--- Mason & LSP
-require("mason").setup()
-require("mason-lspconfig").setup()
-
-require("conform").setup({
-  default_format_opts = { lsp_format = "fallback" },
+-- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
+-- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
+-- See `:help 'clipboard'`
+vim.api.nvim_create_autocmd('UIEnter', {
+  callback = function()
+    vim.o.clipboard = 'unnamedplus'
+  end,
 })
 
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Highlight the line where the cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 0
+
+-- Show <tab> and trailing spaces
+vim.o.list = true
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
+vim.o.confirm = true
+
+-- Code folding
+vim.o.foldmethod = "indent"
+vim.o.foldlevel = 99
+
+-- Tree-Sitter
+vim.treesitter.language.add('python', { path = "/Users/dariog/.config/nvim/parser/python.so" })
+
+-- [[ Set up keymaps ]] See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
+
+-- Use <Esc> to exit terminal mode
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
+vim.keymap.set({ 't', 'i' }, '<A-h>', '<C-\\><C-n><C-w>h')
+vim.keymap.set({ 't', 'i' }, '<A-j>', '<C-\\><C-n><C-w>j')
+vim.keymap.set({ 't', 'i' }, '<A-k>', '<C-\\><C-n><C-w>k')
+vim.keymap.set({ 't', 'i' }, '<A-l>', '<C-\\><C-n><C-w>l')
+vim.keymap.set({ 'n' }, '<A-h>', '<C-w>h')
+vim.keymap.set({ 'n' }, '<A-j>', '<C-w>j')
+vim.keymap.set({ 'n' }, '<A-k>', '<C-w>k')
+vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
+
+-- [[ Basic Autocommands ]].
+-- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
+
+-- Highlight when yanking (copying) text.
+-- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- [[ Create user commands ]]
+-- See `:h nvim_create_user_command()` and `:h user-commands`
+
+-- Create a command `:GitBlameLine` that print the git blame for the current line
+vim.api.nvim_create_user_command('GitBlameLine', function()
+  local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
+  local filename = vim.api.nvim_buf_get_name(0)
+  print(vim.fn.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }))
+end, { desc = 'Print the git blame for the current line' })
+
+-- [[ Add optional packages ]]
+-- Nvim comes bundled with a set of packages that are not enabled by
+-- default. You can enable any of them by using the `:packadd` command.
+
+-- For example, to add the "nohlsearch" package to automatically turn off search highlighting after
+-- 'updatetime' and when going to insert mode
+vim.cmd('packadd! nohlsearch')
