cd5ac41e16342f8326564bf71d1c06c59a7fd268
Giovanni d'Ario <giovanni.dario@roche.com>
2025-10-14 16:01:05 +0200

    Add AucTeX.

--- emacs/init.el
+++ emacs/init.el
@@ -101,13 +101,16 @@
   :mode
   ("\\.epub\\'" . nov-mode))
 
+(use-package auctex
+  :ensure t
+  :defer t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(magit poly-R pyvenv racket-mode scheme-mode sicp toml-mode yaml-mode)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
