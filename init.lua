fb6020b70adc251022599a9942be94de44b31857
Giovanni d'Ario <giovanni.dario@roche.com>
2026-05-21 15:15:53 +0200

    Add code folding to init.lua

--- init.lua
+++ init.lua
@@ -36,6 +36,9 @@ vim.o.cursorline = true -- Highlight the line where the cursor is on.
 vim.o.scrolloff = 0 -- Keep this many screen lines above/below the cursor.
 vim.o.list = true -- Show <tab> and trailing spaces.
 
+vim.o.foldmethod = 'indent'
+vim.o.foldlevel = 99
+
 -- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s). See `:h 'confirm'`
 vim.o.confirm = true
